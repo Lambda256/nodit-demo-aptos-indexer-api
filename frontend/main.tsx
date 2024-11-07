40d241b1844d262df393319527eb9cc76d82dafd
damon.yoon <rim.park@Lai-damonyoon-DK64W97WFN.local>
2024-11-07 16:55:19 +0900

    Fix: remove wallet provider

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -6,7 +6,6 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 // Internal components
 import { Toaster } from "@/components/ui/toaster.tsx";
-import { WalletProvider } from "@/components/WalletProvider.tsx";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -15,14 +14,12 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WalletProvider>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <TooltipProvider delayDuration={100}>
-          <App />
-          <Toaster />
-        </TooltipProvider>
-      </QueryClientProvider>
-    </WalletProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <TooltipProvider delayDuration={100}>
+        <App />
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
